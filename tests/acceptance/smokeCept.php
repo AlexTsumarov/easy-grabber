40f5b284a8e1d64301f7bd85180a57bd2cf24401
Alexandr <Alexandr_Tsumarov1@epam.com>
2015-02-17 19:42:40 +0300

    Travis

--- tests/acceptance/smokeCept.php
+++ tests/acceptance/smokeCept.php
@@ -1,4 +1,8 @@
 <?php 
 $I = new AcceptanceTester($scenario);
-$I->wantTo('perform actions and see result');
-$I->amOnPage('/wp-admin/');
+$I->wantTo('Check WP install');
+$I->amOnPage('/wp-login.php');
+$I->fillField('Username', 'admin');
+$I->fillField('Password','admin');
+$I->click('Log In');
+$I->see('Dashboard');
