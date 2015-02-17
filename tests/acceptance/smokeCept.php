ad26582628efcb54dd7b80e9399d45d7d7b9d831
Alexandr <Alexandr_Tsumarov1@epam.com>
2015-02-17 22:50:02 +0300

    Apache

--- tests/acceptance/smokeCept.php
+++ tests/acceptance/smokeCept.php
@@ -1,7 +1,22 @@
 <?php 
 $I = new AcceptanceTester($scenario);
-$I->wantTo('Check WP install');
+
+$I->wantTo('Smoke test');
+
+$I->amGoingTo('Login');
 $I->amOnPage('/wp-login.php');
 $I->fillField('Username', 'admin');
 $I->fillField('Password','admin');
 $I->click('Log In');
+
+$I->amGoingTo('Activate a plugin');
+$I->amOnPage('/wp-admin/plugins.php');
+$I->click('#easy-grabber .activate a');
+$I->wait(2);
+
+$I->amGoingTo('Run a grabber');
+$I->amOnPage('/wp-admin/admin.php?page=grabber_run');
+$I->click('start');
+
+$I->amGoingTo('Check the results');
+//$I->waitForElement('');
