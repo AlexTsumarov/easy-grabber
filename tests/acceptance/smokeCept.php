cc7cf66069c2ef031b6604b2251268ad8a523700
Alexandr <Alexandr_Tsumarov1@epam.com>
2015-02-17 23:23:46 +0300

    Return back acceptance config

--- tests/acceptance/smokeCept.php
+++ tests/acceptance/smokeCept.php
@@ -11,12 +11,12 @@ $I->click('Log In');
 
 $I->amGoingTo('Activate a plugin');
 $I->amOnPage('/wp-admin/plugins.php');
-$I->click('#easy-grabber .activate a');
-$I->wait(2);
+//$I->click('#easy-grabber .activate a');
+//$I->wait(2);
 
-$I->amGoingTo('Run a grabber');
-$I->amOnPage('/wp-admin/admin.php?page=grabber_run');
-$I->click('start');
+//$I->amGoingTo('Run a grabber');
+//$I->amOnPage('/wp-admin/admin.php?page=grabber_run');
+//$I->click('start');
 
-$I->amGoingTo('Check the results');
+//$I->amGoingTo('Check the results');
 //$I->waitForElement('');
